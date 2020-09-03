acc16949e4e2cc74f92edef50b70a3211474f19d
Thorsteinn <thorsteinn19@ru.is>
2020-09-03 14:07:42 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,4 @@
+#Write in how long the sequence should be
+#Given the first 3 numbers of the sequence 1,2,3 calculate the next
+#value by taking the sum of the three prior numbers in the sequence
+#Repete until you get the desired length of the sequence
